07e081cf164d37b62a12fa100509c0f562cc6f02
Sam Kellett <samkellett@gmail.com>
2015-04-25 13:34:26 +0100

    Correct path for misc scripts.

--- scripts/getdeps.sh
+++ scripts/getdeps.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/bash -e
 
 if [ "$CXX" = "g++" ]; then
-  chmod +x misc/gcc_update.sh
-  ./misc/gcc_update.sh --version 4.9
+  chmod +x misc/travis/cpp/gcc_update.sh
+  ./misc/travis/cpp/gcc_update.sh --version 4.9
 fi
 
 if [ "$CXX" = "clang++" ]; then
-  chmod +x misc/clang_update.sh
-  ./misc/clang_update.sh --version 3.5 --libc++
+  chmod +x misc/travis/cpp/clang_update.sh
+  ./misc/travis/cpp/clang_update.sh --version 3.5 --libc++
 fi
