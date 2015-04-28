9a6d9acb1892c67d2526f682afe9c2c706574aa1
Sam Kellett <samkellett@gmail.com>
2015-04-28 18:46:13 +0100

    Made travis scripts' path relative to getdeps.sh

--- scripts/getdeps.sh
+++ scripts/getdeps.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -e
+readonly DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 if [ "$CXX" = "g++" ]; then
-  chmod +x misc/travis/cpp/gcc_update.sh
-  ./misc/travis/cpp/gcc_update.sh --version 4.9
+  chmod +x $DIR/misc/travis/cpp/gcc_update.sh
+  $DIR/misc/travis/cpp/gcc_update.sh --version 4.9
 fi
 
 if [ "$CXX" = "clang++" ]; then
-  chmod +x misc/travis/cpp/clang_update.sh
-  ./misc/travis/cpp/clang_update.sh --version 3.5 --libc++
+  chmod +x $DIR/misc/travis/cpp/clang_update.sh
+  $DIR/misc/travis/cpp/clang_update.sh --version 3.5 --libc++
 fi
